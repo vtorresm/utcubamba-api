471b8a787038cb99bcf40c64521e0f9854942643
Victor <vatorresm88@gmail.com>
2025-05-16 18:57:11 -0500

    Modificando backend

--- src/db/init_db.py
+++
@@ -1,7 +0,0 @@
-from alembic.config import Config
-from alembic import command
-
-def init_db():
-    # Ejecutar migraciones de Alembic para inicializar la base de datos
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
